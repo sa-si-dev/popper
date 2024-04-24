067d13344421d39651a9e30e03979bc0ca65bfa2
Gonçalo M <goncalo.martins.fct@gmail.com>
2024-04-24 15:20:36 +0100

    Increase version to v1.0.5 (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
-const banner = `Popper v1.0.4
+const banner = `Popper v1.0.5
 https://sa-si-dev.github.io/popper
 Licensed under MIT (https://github.com/sa-si-dev/popper/blob/master/LICENSE)`;
 
